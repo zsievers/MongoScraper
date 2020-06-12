af0a41131d5ba750773f5279b7440d8823ea585c
Zach <zjsievers@gmail.com>
2020-06-11 22:59:42 -0400

    adding to server

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
 var mongoose = require("mongoose");
-var logger = require("morgan");
 
-// scraping tools
-var axios = require("axios");
-var cheerio = require("cheerio");
 
-// require models
-// var db = require("./models");
-
-var PORT = 9000;
+var PORT = process.env.PORT || 9000;
 
 // initalize express
 var app = express();
 
 // Make public a static folder
-app.use(express.static("/public"))
+app.use(express.static("public"))
 
 // Morgan to log requests
 app.use(logger("dev"));
@@ -27,13 +20,14 @@ app.use(express.json());
 
 
 // handlebars to express app
+app.set("views", "./views")
 app.engine("handlebars", exphbs({
     defaultLayout: "main"
 }));
 app.set("view engine", "handlebars");
 
 // deploy to heroku, or locally to mongo mongoESPN db
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoESPN";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoScrape";
 // Connect to the Mongo DB
 mongoose.connect(MONGODB_URI, function(error) {
     if (error) {
@@ -44,6 +38,7 @@ mongoose.connect(MONGODB_URI, function(error) {
     }
 });
 
+require("./config/routes")(app)
 
 // start server
 app.listen(PORT, function() {
