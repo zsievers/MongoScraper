be1f7ba1baa6e32118277314046f641d193ab1a9
Zach <zjsievers@gmail.com>
2020-06-30 08:22:56 -0400

    tweaking server to connect to mongo

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
-var logger = require("morgan");
 var mongoose = require("mongoose");
-var path = require("path");
 // Require all models
 var db = require("./models");
 
@@ -11,36 +9,28 @@ var PORT = process.env.PORT || 3000;
 // Initialize Express
 var app = express();
 
-// Configure middleware
+// Require our routes
+var routes = require("./routes");
 
-// Use morgan logger for logging requests
-app.use(logger("dev"));
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 // Make public a static folder
-app.use(express.static(__dirname + "public"));
+app.use(express.static("public"));
 
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
+// Connect Handlebars to our Express app
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Require our routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// Have every request go through our route middleware
+app.use(routes);
 
 
 // Connect to the Mongo DB
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var db = process.env.MONGODB_URI || "mongodb://localhost/news-scraper";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/news-scraper";
 
-mongoose.connect(db, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI);
 // start server
 app.listen(PORT, function() {
     console.log("App running on port %s. Visit http://localhost:%s/ in your browser", PORT, PORT);
